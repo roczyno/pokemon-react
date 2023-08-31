1bda426d6bf74784d2e6b17efe4ebcf77efe97b4
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-08-31 20:42:48 +0000

    fixed some responsiveness

--- src/context.jsx
+++ src/context.jsx
@@ -16,18 +16,18 @@ const themeReducer = (state, action) => {
 export const ThemeProvider = (props) => {
   const [state, dispatch] = useReducer(themeReducer, INITIAL_STATE);
 
-  // Load theme preference from local storage on app start
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      dispatch({ type: "TOGGLE" }); // Initialize the state with a dispatch
-    }
-  }, []);
+  // // Load theme preference from local storage on app start
+  // useEffect(() => {
+  //   const savedTheme = localStorage.getItem("theme");
+  //   if (savedTheme) {
+  //     dispatch({ type: "TOGGLE" }); // Initialize the state with a dispatch
+  //   }
+  // }, []);
 
-  // Save theme preference to local storage whenever it changes
-  useEffect(() => {
-    localStorage.setItem("theme", JSON.stringify(state.lightMode));
-  }, [state.lightMode]);
+  // // Save theme preference to local storage whenever it changes
+  // useEffect(() => {
+  //   localStorage.setItem("theme", JSON.stringify(state.lightMode));
+  // }, [state.lightMode]);
 
   return (
     <ThemeContext.Provider value={{ state, dispatch }}>
